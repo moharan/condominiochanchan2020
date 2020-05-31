92ba4e7a6bc2f685616f3a93ab186d40e81ae7d3
RangiBecerra <rangibecerra@gmail.com>
2020-05-30 23:04:28 -0400

    webpay

--- src/SectionHome.js
+++ src/SectionHome.js
@@ -6,6 +6,7 @@ import slide02 from './assets/home/02.jpg'
 import slide03 from './assets/home/03.jpg'
 import slide04 from './assets/home/04.jpg'
 import video from './assets/video/chanchan2019.m4v'
+import webpay from './assets/tarifas/webpay.png'
 
 class SectionHome extends React.Component {
   render () {
@@ -97,9 +98,23 @@ class SectionHome extends React.Component {
             <b>(*) Opción de cama supletoria (1 persona adicional) por un recargo de $ 10.000.</b>
           </div>
           <div className='text-conditions'>
-            <p>TEMPORADA BAJA: 01 de Mayo al 10 de Septiembre 2020</p>
-            <p>TEMPORADA MEDIA: 11 de Septiembre al 24 de Diciembre 2020. <b>(Arriendo mínimo 3 noches para Fiestas Patrias)</b></p>
-            <p>TEMPORADA ALTA: 25 de Diciembre 2020 al 6 de Marzo 2021. <b>(Noche de Año Nuevo posee tarifa especial / Arriendo mínimo 3 noches en Enero y 4 noches en Febrero)</b></p>
+            <p>TEMPORADA BAJA: 01 de Mayo al 10 de Septiembre 2020.</p>
+            <p>TEMPORADA MEDIA: 11 de Septiembre al 24 de Diciembre 2020. <b>(Arriendo mínimo 3 noches para Fiestas Patrias).</b></p>
+            <p>TEMPORADA ALTA: 25 de Diciembre 2020 al 6 de Marzo 2021. <b>(Noche de Año Nuevo posee tarifa especial / Arriendo mínimo 3 noches en Enero y 4 noches en Febrero).</b></p>
+          </div>
+          <a href='https://www.webpay.cl/portalpagodirecto/pages/institucion.jsf?idEstablecimiento=36745135' target='_blank' rel='noopener noreferrer'>
+            <img
+              className='webpay-link'
+              src={webpay}
+              alt='webpay'
+            />
+          </a>
+          <div className='text-conditions'>
+            <p><b>Check in:</b> Desde las 15:00 hrs (Temporada Baja y Media) y 15:30 hrs (Temporada Alta)  hasta las 21:00 horas (avisar en caso de llegar posterior a este horario).</p>
+            <p><b>Check out:</b> Hasta las 12:00 horas. Late Check out tiene un costo adicional y previa autorización.</p>
+            <p><b>Condiciones de Reserva:</b> Depósito, transferencia o pago con tarjetas bancarias a través de Webpay.cl del 50% de la estadía por dos o más noches y 100% por una noche.</p>
+            <p><b>Condiciones de Pago:</b> Pago en efectivo, transferencia o tarjetas bancarias de débito/crédito durante el check-in.</p>
+            <p><b>Política de Cancelación:</b> Entre 30 y 10 días antes de la llegada tiene una penalidad del 50% de la reserva y menos de 9 días el 100% de la reserva. Promociones no tienen devolución de reserva.</p>
           </div>
         </div>
       </React.Fragment>
